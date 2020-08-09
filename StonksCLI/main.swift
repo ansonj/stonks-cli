25bf8e953ca979df23f7f14be88766f870e5f87b
Anson Jablinski <a@ansonj.org>
2020-08-09 07:26:07 -0500

    Initial commit of Xcode project

---
+++ StonksCLI/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  StonksCLI
+//
+//  Created by Anson Jablinski on 8/9/20.
+//  Copyright © 2020 Anson Jablinski. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
