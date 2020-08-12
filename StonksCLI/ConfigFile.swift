a43e3ceab6cd39d54d5747b47789543c26c5422e
Anson Jablinski <a@ansonj.org>
2020-08-12 08:08:32 -0500

    Extract config dictionary code for reuse
    - Also marked the utility functions as private

--- StonksCLI/ConfigFile.swift
+++ StonksCLI/ConfigFile.swift
@@ -24,24 +24,31 @@ struct ConfigFile {
     
     // MARK: Properties
     
-    let k_iexCloudApiKey = "iexCloudApiKey"
+    private func getString(forKey key: String) -> String {
+        let configDictionary = readConfigDictionary()
+        return configDictionary[key] ?? ""
+    }
+    
+    private func setString(_ newValue: String, forKey key: String) {
+        var configDictionary = readConfigDictionary()
+        configDictionary[key] = newValue
+        setConfigDictionary(configDictionary)
+    }
     
     // FIXME: Use property wrappers instead?
     // https://www.swiftbysundell.com/articles/property-wrappers-in-swift/
-    func iexCloudApiKey() -> String {
-        let configDictionary = readConfigDictionary()
-        return configDictionary[k_iexCloudApiKey] ?? ""
-    }
     
+    private let k_iexCloudApiKey = "iexCloudApiKey"
+    func iexCloudApiKey() -> String {
+        return getString(forKey: k_iexCloudApiKey)
+    }
     func setIexCloudApiKey(_ newValue: String) {
-        var configDictionary = readConfigDictionary()
-        configDictionary[k_iexCloudApiKey] = newValue
-        setConfigDictionary(configDictionary)
+        setString(newValue, forKey: k_iexCloudApiKey)
     }
     
     // MARK: Utilities
     
-    func readConfigDictionary() -> [String: String] {
+    private func readConfigDictionary() -> [String: String] {
         guard let jsonFileContents = try? Data(contentsOf: configFileUrl) else {
             Prompt.exitStonks(withMessage: "Couldn't load config file.")
         }
@@ -51,7 +58,7 @@ struct ConfigFile {
         return configDictionary
     }
     
-    func setConfigDictionary(_ newValue: [String: String]) {
+    private func setConfigDictionary(_ newValue: [String: String]) {
         guard let newData = try? JSONEncoder().encode(newValue) else {
             Prompt.exitStonks(withMessage: "Couldn't encode updated JSON data.")
         }
