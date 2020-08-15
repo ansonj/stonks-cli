74916375ffb7c29c63f00807e03148ce3aa7131c
Anson Jablinski <a@ansonj.org>
2020-08-15 15:20:05 -0500

    Show error message if main menu selection was bad

--- StonksCLI/Flows/MainFlow.swift
+++ StonksCLI/Flows/MainFlow.swift
@@ -2,19 +2,27 @@ struct MainFlow: Flow {
     let configFile: ConfigFile
     
     func run() {
+        var lastInputErrorMessage: String? = nil
         while true {
             printActiveTable()
             // TODO: Print buying power checksum
             // TODO: Print pending buys list
             printMainMenu()
+            let promptString: String
+            if lastInputErrorMessage != nil {
+                promptString = "Choose action (\(lastInputErrorMessage!)):"
+                lastInputErrorMessage = nil
+            } else {
+                promptString = "Choose action:"
+            }
             // TODO: Gracefully handle input of Ctrl+D
-            let selection = Prompt.readString(withMessage: "Choose action:")
+            let selection = Prompt.readString(withMessage: promptString)
             switch selection.first {
             case "b":
                 let buy = BuyFlow(configFile: configFile)
                 buy.run()
             default:
-                print("Invalid entry '\(selection)'.")
+                lastInputErrorMessage = "'\(selection)' is not an option"
             }
         }
     }
@@ -22,10 +30,12 @@ struct MainFlow: Flow {
     private func printActiveTable() {
         // TODO: Implement this!
         print("[Active table WIP]")
+        print()
     }
     
     private func printMainMenu() {
         print("Main menu")
         print("    (b)uy")
+        print()
     }
 }
