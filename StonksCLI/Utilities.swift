e4a1adfe4bbe2b75dcc5782de7c6d0a6559b4aa0
Anson Jablinski <a@ansonj.org>
2020-08-25 07:51:48 -0500

    Fix bug with getting days between dates

--- StonksCLI/Utilities.swift
+++ StonksCLI/Utilities.swift
@@ -2,7 +2,14 @@ import Foundation
 
 struct Utilities {
     static func daysBetween(_ start: Date, and end: Date) -> Int {
-        // FIXME: There is a bug here; need to ensure these dates have the same time
-        return Calendar.current.dateComponents([.day], from: start, to: end).day ?? -1
+        // https://stackoverflow.com/questions/24723431/#comment58538259_28163560
+        let cal = Calendar.current
+        guard let noonStart = cal.date(bySettingHour: 12, minute: 0, second: 0, of: start) else {
+            Prompt.exitStonks(withMessage: "Couldn't set start date to noon: \(start)")
+        }
+        guard let noonEnd = cal.date(bySettingHour: 12, minute: 0, second: 0, of: end) else {
+            Prompt.exitStonks(withMessage: "Couldn't set end date to noon: \(end)")
+        }
+        return cal.dateComponents([.day], from: noonStart, to: noonEnd).day ?? -1
     }
 }
